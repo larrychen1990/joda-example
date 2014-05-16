74c1e00e946914f2496cd03940041996d56c0d7e
LarryChen1990 <larrychen1990@163.com>
2014-05-16 10:31:59 +0800

    de

--- src/com/makotogroup/joda/custom/LoggerTest.java
+++ src/com/makotogroup/joda/custom/LoggerTest.java
@@ -1,16 +1,31 @@
 package com.makotogroup.joda.custom;
 
 import org.apache.log4j.Logger;
-
+import org.apache.log4j.spi.LoggerFactory;
 
 public class LoggerTest {
-  private static Logger log=Logger.getLogger(LoggerTest.class);
+  private  Logger log=Logger.getLogger(getClass());
   
   public static void main(String[] args) {
-    log.fatal("this is fatal log.");
-    log.error("this is error log.");
-    log.warn("this is warn log.");
-    log.info("this is info log.");
-    log.debug("this is debug log.");
+    LoggerTest t=new LoggerTest();
+    t.log.fatal("this is fatal log.");
+    t.log.error("this is error log.");
+    t.log.warn("this is warn log.");
+    t.log.info("this is info log.");
+    t.log.debug("this is debug log.");
   }
 }
+
+
+//public class LoggerTest {
+//  
+//  private static Logger log = Logger.getLogger(LoggerTest.class);
+//  
+//  public static void main(String[] args) {
+//    log.fatal("this is fatal log.");
+//    log.error("this is error log.");
+//    log.warn("this is warn log.");
+//    log.info("this is info log.");
+//    log.debug("this is debug log.");
+//  }
+//}
